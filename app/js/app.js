d00b497329d1282ab72b611cec86b2e513274e52
aleeex <i can't believe it's not windows>
2014-09-22 20:30:09 -0700

    commenting

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 	/*global vars */
-	/*allowing the enter key to trigger submit*/
 	//unit test
 	//alert('yo');
+
+	/*allowing the enter key to trigger submit*/
 	function enterKey() {
 		$('#the-input').on('keydown', function(e){
 			if(e.which==13){
@@ -12,6 +13,7 @@ $(document).ready(function() {
 	}
 	enterKey();
 
+	/*grabs the user input and sets it to a var*/
 	function displayValue() {
 		var userInput = $('#the-input').val();
 		alert(userInput);
