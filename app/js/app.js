7a99e6a1c54d019162f49c6f64bb5d39e48a3195
aleeex <i can't believe it's not windows>
2014-09-22 20:15:36 -0700

    tried a unit test to make sure the function ran correctly. it didn't

--- app/js/app.js
+++ app/js/app.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
 	/*global vars */
 	/*allowing the enter key to trigger submit*/
 	function enterKey() {
-		$("#the-input").on('keydown', function(e){
+		$('#the-input').on('keydown', function(e){
 			if(e.which==13){
 				alert("yo");
 			}
-		})
+		});
 	}
 });
