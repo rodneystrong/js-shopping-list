008abe39f4be4b738bc3000df0cecefa60a83e48
aleeex <i can't believe it's not windows>
2014-09-22 20:18:01 -0700

    did another unit test just for the doc.ready function. OH SHIT I FORGOT TO CALL THE FUNCTION! duh.. wow. derp.

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	/*global vars */
 	/*allowing the enter key to trigger submit*/
+	//unit test
+	//alert('yo');
 	function enterKey() {
 		$('#the-input').on('keydown', function(e){
 			if(e.which==13){
@@ -8,4 +10,5 @@ $(document).ready(function() {
 			}
 		});
 	}
+	enterKey();
 });
